026f5bada2d254f71e2bbfbaddb271ddc71a8680
Akshat Gupta <akshat1499@gmail.com>
2022-07-26 10:02:20 +0530

    Added User Model

---
+++ Backend/src/main/java/com.example.foodapp/enumclasses/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.foodapp.enumclasses;
+
+public enum UserRole {
+    ADMIN, RMANAGER, CUSTOMER
+}
--- Backend/src/main/java/com.example.foodapp/model/User.java
+++ Backend/src/main/java/com.example.foodapp/model/User.java
@@ -1,4 +1,98 @@
 package com.example.foodapp.model;
 
+import com.example.foodapp.enumclasses.UserRole;
+import com.mongodb.lang.NonNull;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NonNull
+    private String email;
+
+    private String userName;
+    private UserRole role;
+    private String password;
+    private String salt;
+    private Integer phone;
+    private String address;
+    private String jwt;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    @NonNull
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NonNull String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
 }
