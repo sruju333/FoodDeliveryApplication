8442478c0583077cb1d3b52f727d3ad1c079abed
Akshat Gupta <akshat1499@gmail.com>
2022-07-26 09:07:40 +0530

    Added Config Files

---
+++ Backend/src/main/java/com.example.foodapp/FoodApplication.java
@@ -0,0 +1,11 @@
+package com.example.foodapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FoodApplication.class);
+    }
+}
