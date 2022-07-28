9905b9fa213df5e7bfbec5440b30642f51479a8b
Sourabh Chauhan <kschauhan400@gmail.com>
2022-07-28 13:00:27 +0530

    Signup login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-
-
-function App() {
+// import {Container} from 'react-bootstrap'
+import React,{Component,useState} from 'react';
+//import './App.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes,Route} from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+function App(){
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+      </Routes>
+    </BrowserRouter>
+   </>
   );
+   
 }
 
 export default App;
--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,15 +1,17 @@
 import React,{useState} from 'react';
-import { Navigate, useNavigate ,Link} from 'react-router-dom';
 import "./Register.css";
+
+
 
 function Register(){
     
     const [user,setUser]=useState({
-        name:"",
+        username:"",
         email:"",
         phonenumber:"",
         password:"",
-        reEnterPassword:""
+        role:"",
+        address:""
 
     })
     const handleChange =(e)=>{
@@ -24,18 +26,26 @@ function Register(){
         console.log(user);
     }
    
+    
+
     return(
         
         <form className="register" onSubmit={registerMe}>
         <h1>Register</h1>
-        <input type="text" name="name" value={user.name} placeholder="Enter your Name" onChange={handleChange}></input>
+        <input type="text" name="username" value={user.username} placeholder="Enter your username" onChange={handleChange}></input>
         <input type="text" name="email" value={user.email}placeholder="Enter your email" onChange={handleChange}></input>
         <input type="phone" name="phonenumber" value={user.phonenumber}placeholder="Enter your Phone Number"onChange={handleChange}></input>
         <input type="password" name="password" value={user.password} placeholder="Enter your password" onChange={handleChange}></input>
-        <input type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder="Re-enter your password" onChange={handleChange}></input>
+        <input type="text" name="address" value={user.address} placeholder="Enter your address" onChange={handleChange}></input>
+        <select value={user.role} name="role" onChange={handleChange}>
+        <option value="Enter your role">Enter your Role</option>
+        <option value="user">User</option>
+            <option value="rmanager">RManager</option>
+        </select>
         <button className="btn btn-primary my-3 h4">Register</button>
         <div><a href='/login'>Already have an account</a></div>
         </form>
+       
     )
 }
 export default Register;
